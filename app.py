e50f81bb4f29c7177634711413a9728883920cb2
Gustavo da Silva Franchin - GUUUHYT <106678386+GustavoSFG@users.noreply.github.com>
2024-10-03 09:03:37 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,79 @@
 import os
 
-print('Franchin Multimarcas')
+carros = []
 
-print('1. Cadastro de Veiculos')
-print('2. Listar Renavam e Chassi')
-print('3. Ativar Anuncios de Veiculos')
-print('4. Sair da Aplicação')
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print('')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-print('Você escolheu a opção: ', opcao_escolhida)
+def retorna_menu_principal():
+     input('\n Digite qualquer tecla para voltar ao menu principal')
+     main()
 
-if opcao_escolhida == 1: 
-    print('Cadastro de Veiculo')
-elif opcao_escolhida == 2:
-    print('Listar  Renavam e Chassi')
+def mostra_titulo():
+    print("""
+          
+𝑭𝑹𝑨𝑵𝑪𝑯𝑰𝑵 𝑴𝑼𝑳𝑻𝑰𝑴𝑨𝑹𝑪𝑨𝑺
+          
+        """)
     
-elif opcao_escolhida == 3:
-    print('Ativar Anuncios de Veiculos')
-else:
-    print('Saindo da Aplicação')
+def mostra_escolha():
+    print('1. Cadastro de carros')
+    print('2. Listar carros')
+    print('3. Ativar carros')
+    print('4. Sair da aplicação')
+
+def escolhe_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print('Você escolheu a opção: ', opcao_escolhida)
+
+        if opcao_escolhida == 1:
+            cadastrar_carros()
+        elif opcao_escolhida == 2:
+            mostrar_carros
+        elif opcao_escolhida == 3:
+            print('Ativar/desativar carro')
+        elif opcao_escolhida == 4:
+            finalizar_programa()
+        else:
+            opcao_invalida()
+
+    except:
+         opcao_invalida()
+
+def cadastrar_carros():
+    exibir_subtitulo('Cadastrar carros')
+
+    nome_carro = input('Digite o nome do carro: ')
+    carros.append(nome_carro)
+    print(f'{nome_carro} foi adicionado a lista de carros')
+    input('Digite qualquer tecla para voltar')
+    main()
+
+def mostrar_carros():
+    exibir_subtitulo('Listar carros')
     
+    for carro in carros:
+         print(f' - carro')
+
+    input('Digite qualquer tecla para voltar')
+    main()
+
+def finalizar_programa():
+        os. system('cls')
+        print('Finalizando programa')
+
+def opcao_invalida():
+        print('Este caracter não é permitido')
+        input('Digite qualquer tecla')
+        main()
+
+def main():
+    mostra_titulo()
+    mostra_escolha()
+    escolhe_opcao()
+
+if __name__ == '__main__':
+    main()
